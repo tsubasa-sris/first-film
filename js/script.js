910452d4f29ac22bda68bb43aa5294ba0b321845
Tsubasa Shiraishi <tsubasa@TsubasanoMacBook-Pro.local>
2021-07-27 00:49:07 +0900

    add index page

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-window.onload = function() {
-  const spinner = document.getElementById('loading');
-  spinner.classList.add('loaded');
-}
-
-$(function(){
-    $('.js-modal-open').each(function(){
-        $(this).on('click',function(){
-            var target = $(this).data('target');
-            var modal = document.getElementById(target);
-            $(modal).fadeIn(500);
-            return false;
-        });
-    });
-    $('.js-modal-close').on('click',function(){
-        $('.js-modal').fadeOut(500);
-        return false;
-    });
-});
+var random = Math.floor(Math.random() * $('.item').length);
+$('.item').hide().eq(random).show();
